ab107b922de7b913d20134be96d3f9e48b873ca8
asavage8 <ams467@pitt.edu>
2017-03-14 11:21:26 -0400

    working

--- assignment4.js
+++ assignment4.js
@@ -1,18 +1,8 @@
-// The anonymous function below will fire on page load
-
-// Some things to consider
-// $.ajax(); to make your requests a little easier. Or the vanilla js way, it's up to you.
-// $.on(); for event handling
-// Remember, selecting elements in jQuery is like selecting them in CSS
-// You'll probably have to manipulate some strings
-// some jQuery functions to help display results
-// $.show(), $.hide(), $.slideup(), $.slidedown(), $.fadein(), $.fadeout()
-// Add content from requests with something like
-// $.html(), $.text(), etc.
-// keyup events could be helpful to get value of field as the user types
-
-(function() {
+//Predictive Search
+$(document).ready(function() {
   // Magic!
+  var $search;
+  var result= [];
   $.ajax({
 		url: 'http://www.mattbowytz.com/simple_api.json',
 		type: 'GET',
@@ -21,13 +11,44 @@
 		success: function(data) {
       $search = $.merge(data.data.interests, data.data.programming);
       $search.sort();
-       console.log($search);
-       console.log(data);
-    }
+      }
   });
-  $('.flexsearch-input').on('keyup', function(e){
-    var input= $(this).val();
-    console.log(input);
-  })
-
-})();
+  $('.flexsearch-input').on('keyup input', function(e) {
+		var input = $(this).val();
+		if (input) {
+			result.length = 0;
+			var regex = new RegExp("\^" + input, "i");
+			$.each($search, function(index, value) {
+				if (regex.test(value) === true) {
+						result.push('<li><a href="http://www.google.com/search?q=' + value +'">' + value + '</a></li>');
+				}
+			});
+			if (result.length == 0) {
+				result.push("<li>No search result</li>");
+			}
+			$('.flexsearch-list li').remove();
+			$.each(result, function(index, value) {
+				$('.flexsearch-list').append(value);
+			});
+			if ($('.flexsearch-result-wrapper').is(":hidden")) {
+				$('.flexsearch-result-wrapper').show();
+			}
+		}
+		else {
+			if ($('.flexsearch-result-wrapper').is(":visible")) {
+				$('.flexsearch-result-wrapper').hide();
+			}
+		}
+	});
+	$('.flexsearch-form').submit(function(e) {
+		if (typeof result !== "undefined" && result.length > 0) {
+			var url = result[0];
+			if (url.slice(0, 12) == "<li><a href=") {
+				url = url.slice(13, url.indexOf('"><'));
+				e.preventDefault();
+				document.location.href = url;
+			}
+		}
+	});
+	console.log('Keepin\'n it clean with an external script!');
+}); // main
